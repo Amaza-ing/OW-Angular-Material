d9fbad0b058b5e42bc8a24c56a1049c8cce0937a
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 13:13:14 +0100

    menus

--- src/app/components/menus/menus.component.ts
+++ src/app/components/menus/menus.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menus',
-  imports: [MatMenuModule, MatButtonModule],
+  imports: [MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './menus.component.html',
   styleUrl: './menus.component.css',
 })
 export class MenusComponent {
   showAlert() {
-    alert("Hola a todos!");
+    alert('Hola a todos!');
   }
 }
