07e92b68febc7eb6307ca248061f4131b3263159
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 15:47:12 +0100

    forms

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  articleForm = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl(''),
+    author: new FormControl(''),
+  });
 
+  handleSubmit() {
+    console.log(this.articleForm.value);
+
+    this.articleForm.reset();
+  }
 }
