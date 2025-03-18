c21b300d7746752fbcd5a160b6595dd29545d8dd
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 16:28:51 +0100

    autocomplete

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -8,6 +8,12 @@ import {
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AsyncPipe } from '@angular/common';
+
+export interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-form',
@@ -16,6 +22,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatAutocompleteModule,
+    AsyncPipe,
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
@@ -26,6 +34,31 @@ export class FormComponent {
     content: new FormControl('', [Validators.required]),
     author: new FormControl('', [Validators.required, Validators.email]),
   });
+
+  options: User[] = [{ name: 'Mary' }, { name: 'Shelley' }, { name: 'Igor' }];
+  filteredOptions: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      })
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.name.toLowerCase().includes(filterValue)
+    );
+  }
 
   handleSubmit() {
     console.log(this.articleForm.value);
