c2509526a2512f813cbae5c65e0c02186ac57d82
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 11:59:07 +0100

    grid

--- src/app/components/grid-layout/grid-layout.component.ts
+++ src/app/components/grid-layout/grid-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { Tile } from '../../models/Tile';
 
@@ -8,11 +8,24 @@ import { Tile } from '../../models/Tile';
   templateUrl: './grid-layout.component.html',
   styleUrl: './grid-layout.component.css',
 })
-export class GridLayoutComponent {
+export class GridLayoutComponent implements OnInit {
+  breakpoint = 500;
+  cols = 4;
+
   tiles: Tile[] = [
     { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
     { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
     { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
     { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
   ];
+
+  ngOnInit(): void {
+    if (window.innerWidth < this.breakpoint) {
+      this.cols = 1;
+      this.tiles.forEach((tile) => {
+        tile.cols = 1;
+        tile.rows = 1;
+      });
+    }
+  }
 }
