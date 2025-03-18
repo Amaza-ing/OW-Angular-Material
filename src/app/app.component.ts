92d520f9640e0952ce2d3db55729675e596df08d
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 15:44:21 +0100

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from "./components/form/form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
