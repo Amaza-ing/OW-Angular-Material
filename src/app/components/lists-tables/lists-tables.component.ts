f6848bc4a61e5ec4056d106205c0de1a4139040a
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 13:30:55 +0100

    lists & tables

--- src/app/components/lists-tables/lists-tables.component.ts
+++ src/app/components/lists-tables/lists-tables.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-lists-tables',
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './lists-tables.component.html',
-  styleUrl: './lists-tables.component.css'
+  styleUrl: './lists-tables.component.css',
 })
 export class ListsTablesComponent {
-
+  typesOfShoes: string[] = [
+    'Boots',
+    'Clogs',
+    'Loafers',
+    'Moccasins',
+    'Sneakers',
+  ];
 }
