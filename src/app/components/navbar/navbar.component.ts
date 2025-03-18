3f3be644e7bbcffb3d74bc73c73e74f077b70e7b
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 14:19:47 +0100

    navbars

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatButtonModule, MatToolbarModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  linkStyle = {
+    color: 'white',
+  };
 }
