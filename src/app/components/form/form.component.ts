fbb669a88c0254486606dce9545b916ade6c9f1f
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 15:58:48 +0100

    forms

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
