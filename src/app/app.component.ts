0a428a5d7a6602563f9de1be0b9cba8d30a068cf
Adrián Maza <adri_mv1@hotmail.com>
2025-03-18 16:48:31 +0100

    tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { FormComponent } from "./components/form/form.component";
+import { TablesComponent } from "./components/tables/tables.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FormComponent],
+  imports: [HeaderComponent, TablesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
