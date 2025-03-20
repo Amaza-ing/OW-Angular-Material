549fb109906f494041cbf85a2e8e487c24893630
Adrián Maza <adri_mv1@hotmail.com>
2025-03-20 16:22:59 +0100

    tooltips

--- src/app/components/tooltips/tooltips.component.ts
+++ src/app/components/tooltips/tooltips.component.ts
@@ -7,4 +7,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   templateUrl: './tooltips.component.html',
   styleUrl: './tooltips.component.css',
 })
-export class TooltipsComponent {}
+export class TooltipsComponent {
+  tooltipText =
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam inventore ad veritatis, repellendus molestias dolorem recusandae ipsam quisquam autem est laborum quasi eos? Quibusdam aliquam officiis similique numquam ex. Fugit?';
+  
+  showDelay = 500;
+  hideDelay = 2000;
+}
