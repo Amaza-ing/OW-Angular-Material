84050f5cdeb53b675130355a42a9976b54fb1cc2
Adrián Maza <adri_mv1@hotmail.com>
2025-03-20 16:19:18 +0100

    tooltips

--- src/app/components/tooltips/tooltips.component.ts
+++ src/app/components/tooltips/tooltips.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-tooltips',
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './tooltips.component.html',
-  styleUrl: './tooltips.component.css'
+  styleUrl: './tooltips.component.css',
 })
-export class TooltipsComponent {
-
-}
+export class TooltipsComponent {}
