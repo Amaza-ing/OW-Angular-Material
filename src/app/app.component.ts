10aeeb8471502bd3634ddb247ef3e985daa7b00b
Adrián Maza <adri_mv1@hotmail.com>
2025-03-20 13:14:32 +0100

    dialogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { TablesComponent } from "./components/tables/tables.component";
+import { DialogsComponent } from "./components/dialogs/dialogs.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, TablesComponent],
+  imports: [HeaderComponent, DialogsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
