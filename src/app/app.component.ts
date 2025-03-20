8631ad450f85335e56037a529063987ff1e554af
Adrián Maza <adri_mv1@hotmail.com>
2025-03-20 16:16:41 +0100

    tooltips

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { OpenDialogComponent } from "./components/open-dialog/open-dialog.component";
+import { TooltipsComponent } from "./components/tooltips/tooltips.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, OpenDialogComponent],
+  imports: [HeaderComponent, TooltipsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
