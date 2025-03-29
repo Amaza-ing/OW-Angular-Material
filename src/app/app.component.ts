ea20709865ddf871a0ebd09e2f38cf254d6a7be2
Adrián Maza <adri_mv1@hotmail.com>
2025-03-29 13:43:56 +0100

    final-03 footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { FeaturesComponent } from "./components/features/features.component";
 import { TestimonialComponent } from "./components/testimonial/testimonial.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HeroComponent, FeaturesComponent, TestimonialComponent],
+  imports: [HeaderComponent, HeroComponent, FeaturesComponent, TestimonialComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
