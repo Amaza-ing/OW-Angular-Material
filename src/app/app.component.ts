64484c94e7b05796f915b1b31f9f78439f22e315
Adrián Maza <adri_mv1@hotmail.com>
2025-03-29 12:11:50 +0100

    final-01 clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { OpenDialogComponent } from "./components/open-dialog/open-dialog.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, OpenDialogComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
