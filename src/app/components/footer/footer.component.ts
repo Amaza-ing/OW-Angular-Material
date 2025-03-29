ea395592cc87e56b9cc924b515ac5ed6eb73b821
Adrián Maza <adri_mv1@hotmail.com>
2025-03-29 13:52:51 +0100

    final-03 form

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
