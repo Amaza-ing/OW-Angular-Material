51ad365b7577818e6e6c0753a07d839d082f00a1
Adrián Maza <adri_mv1@hotmail.com>
2025-03-29 13:56:43 +0100

    tooltip

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-footer',
@@ -13,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTooltipModule,
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
